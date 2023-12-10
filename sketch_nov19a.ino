994eb49d934d774db7704ef633bd197c033c5fae
NTLDev <114810424+NtLitvinenko@users.noreply.github.com>
2023-12-10 14:43:42 +0700

    Update sketch_nov19a.ino

--- sketch_nov19a.ino
+++ sketch_nov19a.ino
@@ -1,8 +1,12 @@
 // y 8, x 5 (custom symbol)
 
 #include <Wire.h> 
+//#include "notes.h"
+
 #include <LiquidCrystal_I2C.h>
 LiquidCrystal_I2C lcd(0x27,16,2);  // Устанавливаем дисплей
+
+bool rbos = false;
 byte std1[] = {
   B11111,
   B10001,
@@ -64,6 +68,7 @@ byte cactus[] = {
   B00100
 };
 
+const short bPin = 11;
 short bird_pos = 16;
 short bird_y = random(0, 2);
 
@@ -77,14 +82,19 @@ void setup() {
   lcd.createChar(4, bird);
   lcd.createChar(5, cactus);
 
-  lcd.backlight(); // Turn on the display backlight
+  lcd.backlight();
 
-  pinMode(2, INPUT); // Assuming pin 2 is for white button
-  pinMode(3, INPUT); // Assuming pin 3 is for blue button
-  pinMode(4, INPUT); // Assuming pin 4 is for red button
+  pinMode(2, INPUT); // 
+  pinMode(3, INPUT); // 
+  pinMode(4, INPUT); // 
+  tone(bPin, 9600, 1000);
+  tone(bPin, 96000, 1000);
+  tone(bPin, 9600, 1000);
+  tone(bPin, 96000, 1000);
 }
 
 void gOver() {
+  tone(bPin, 900, 3000);
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("Game over!");
@@ -92,53 +102,56 @@ void gOver() {
 }
 short cactus_pos=19;
 short fuss = 0;
+bool gamePaused = false;
+
 void loop() {
   static short costume_std = 0;
   static short costume_lay = 0;
 
   bool isWhite = digitalRead(2);
   bool isBlue = digitalRead(3);
-  bool isTStopped = digitalRead(4);
+  bool isPauseButtonPressed = digitalRead(4);
 
-  if (!isTStopped) {
-    // Clear the display
+  if (isPauseButtonPressed) {
+    delay(300);
+    lcd.setCursor(0, 0);
+    lcd.print("Pause");
+    
+    while (!digitalRead(4)) {
+    }
+    gamePaused = !gamePaused; 
+    delay(200); 
+  }
+  else {
+
     lcd.clear();
-
-    // Update bird and cactus positions
     bird_pos--;
     cactus_pos--;
-
-    // Check collision between bird and player
     if (bird_pos == 0) {
-      if ((isWhite && bird_y == 1) || (isBlue && bird_y == 0) || (!isWhite && bird_y == 0)) {
+      if ((isWhite && bird_y == 1) || (isBlue && bird_y == 0) || (!isWhite && bird_y == 0) || (!isWhite && !isBlue && bird_y==1)) {
         gOver();
       } else {
-        // Respawn bird at new coordinates after a level ends
         bird_pos = 16;
         bird_y = random(0, 2);
       }
     }
 
-    // Check collision between player and cactus
     if ((cactus_pos == 0) && (!isBlue)) {
       gOver();
     }
     else {
-      // Respawn cactus at new coordinates after passing
       if (cactus_pos < 0) {
       
         cactus_pos = random(bird_pos+4, bird_pos+12);
       }
     }
 
-    // Draw bird and cactus on the LCD
     lcd.setCursor(bird_pos, bird_y);
     lcd.write(4);
 
     lcd.setCursor(cactus_pos, 1);
     lcd.write(5);
     if (!isBlue) {fuss = 0; }
-    // Player actions (jump or lay)
     if (isWhite) {
       lcd.setCursor(0, 1);
       if (costume_lay == 1) {
@@ -170,9 +183,5 @@ void loop() {
       }
     }
     delay(250);
-  } else {
-    lcd.setCursor(0, 0);
-    lcd.print("Pause");
-    delay(500);
   }
 }
